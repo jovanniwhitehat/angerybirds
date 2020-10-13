0227174157837913635d5a7e25e1dbc9bb5bc10b
jovanniwhitehat <jovanni.larin1@gmail.com>
2020-10-13 15:53:15 -0700

    adding hill

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    hill = new Ground(180, 300, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -40,10 +40,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    Engine.update(engine)
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +57,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    hill.display();
 }
